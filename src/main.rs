fbb77be52f33cf8b439fdf7d816f0ad175f4103e
SolarOmni <johnathan.crocker@outlook.com>
2019-08-19 16:15:06 -0500

    Dungeon fov implemented

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,16 @@ use tcod::{colors, OffscreenConsole};
 use tcod::console::*;
 use tcod::input::Key;
 use tcod::input::KeyCode::*;
+use tcod::map::{FovAlgorithm, Map as FovMap};
+use tcod::Color;
+
+pub const FOV_ALGO: FovAlgorithm = FovAlgorithm::Basic;
 
 mod object;
 use object::Object;
 mod map;
 use map::Map;
 mod tile;
-mod fov;
 
 //main method
 fn main() {
@@ -26,7 +29,6 @@ fn main() {
 
     //make map
     let (map, (player_x, player_y)) = map::make_map();
-    
     // make player
     let mut player = Object::new(player_x, player_y, '@', colors::WHITE);
 
@@ -35,11 +37,26 @@ fn main() {
     
     //place npcs into array
     let objects = [npc1];
-
+    let mut map = map;
     //Main loop
     while !root.window_closed() {
+        let mut previous_player_position = (-1, -1);
 
-        render_all(&mut root, &mut con, &mut player, &objects, &map);
+        let mut fov_map = FovMap::new(map::MAP_WIDTH, map::MAP_HEIGHT);
+        for y in 0..map::MAP_HEIGHT {
+            for x in 0..map::MAP_WIDTH {
+                fov_map.set(
+                    x,
+                    y,
+                    !map[x as usize][y as usize].block_sight,
+                    !map[x as usize][y as usize].blocked,
+                );
+            }
+        }
+        let fov_recompute = previous_player_position != (player.x, player.y);
+        render_all(&mut root, &mut con, &mut player, &objects, &mut map, &mut fov_map, fov_recompute);
+
+        previous_player_position = (player.x, player.y);
 
         let handle = handle_keys(&mut root, &objects, &mut player, &map);
         if handle {
@@ -49,28 +66,53 @@ fn main() {
 }
 
 //main draw function
-fn render_all(root : &mut Root, con : &mut OffscreenConsole, player : &mut Object, objects : &[Object], map : &Map) {
+fn render_all(root : &mut Root, con : &mut OffscreenConsole, player : &mut Object, objects : &[Object], map : &mut Map, fov : &mut FovMap, frc : bool) {
     //clear the offscreen canvas with black
     con.set_default_foreground(colors::BLACK);
     con.clear();
+    
+    //If we should recompute fov
+    if frc {
+        fov.compute_fov(player.x, player.y, map::TORCH_RADIUS, map::FOV_LIGHT_WALLS, FOV_ALGO);
+    }
 
     //draw walls
     for y in 0..map::MAP_HEIGHT {
         for x in 0..map::MAP_WIDTH {
-            //if tile blocks sight? Then it must be a wall?
+            let visible = fov.is_in_fov(x, y);
             let wall = map[x as usize][y as usize].block_sight;
-            if wall {
-                con.set_char_background(x, y, map::COLOR_DARK_WALL, BackgroundFlag::Set);
+            let color : Color;
+            if visible && wall {
+                color = map::COLOR_LIGHT_WALL;
+            }
+            else if !visible && wall {
+                color = map::COLOR_DARK_WALL;
+            }
+            else if visible && !wall {
+                color = map::COLOR_LIGHT_GROUND;
+            }
+            else if !visible && !wall {
+                color = map::COLOR_DARK_GROUND;
             }
             else {
-                con.set_char_background(x, y, map::COLOR_DARK_GROUND, BackgroundFlag::Set);
+                color = colors::WHITE;
+            }
+
+            let explored = &mut map[x as usize][y as usize].explored;
+            if visible {
+                *explored = true;
+            }
+            if *explored {
+                con.set_char_background(x, y, color, BackgroundFlag::Set);
             }
         }
     }
 
     //draw objects
     for object in objects {
-        object.draw(con);
+        if fov.is_in_fov(object.x, object.y) {
+            object.draw(con);
+        }
     }
 
     //draw player
--- src/map.rs
+++ src/map.rs
@@ -13,8 +13,13 @@ pub const MAX_ROOM_SIZE : i32 = 12;
 pub const MIN_ROOM_SIZE : i32 = 5;
 pub const MAX_ROOMS : i32 = 50;
 
+pub const FOV_LIGHT_WALLS: bool = true;
+pub const TORCH_RADIUS: i32 = 10;
+
 pub const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
+pub const COLOR_LIGHT_WALL: Color = Color { r: 130, g: 110, b: 50 };
 pub const COLOR_DARK_GROUND: Color = Color { r: 90, g: 90, b: 90 };
+pub const COLOR_LIGHT_GROUND: Color = Color { r: 200, g: 180, b: 50 };
 
 #[derive(Clone, Copy, Debug)]
 struct Rect {
